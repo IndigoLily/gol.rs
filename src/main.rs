c2ab06e615a5bfd5b6e5bf6c05c6edd86340f95b
lily <yulex.42@gmail.com>
2020-09-09 03:56:07 -0300

    Added ability to control speed with +/- keys, and restart with Enter key without closing the program. The chance of a cell being alive at the start is now randomly set every restart

--- src/main.rs
+++ src/main.rs
@@ -22,53 +22,68 @@ fn main() {
     let (tx, rx) = channel();
     let key_handler = thread::spawn(move || {
         for c in stdin.keys() {
-            if let Key::Ctrl('c') | Key::Char('q') = c.unwrap() {
-                tx.send("quit").unwrap();
-                break;
+            match c.unwrap() {
+                Key::Ctrl('c') | Key::Char('q') => {
+                    tx.send("quit").unwrap();
+                    break;
+                },
+                Key::Char('+') => tx.send("speed+").unwrap(),
+                Key::Char('-') => tx.send("speed-").unwrap(),
+                Key::Char('\n') => tx.send("restart").unwrap(),
+                _ => (),
             }
         }
     });
 
-    let mut cells = CellGrid::new();
-    let (w, h) = termion::terminal_size().unwrap();
-    let mut rng = thread_rng();
-    for x in (1..=w).map(|x| x as isize) {
-        for y in (1..=h).map(|x| x as isize) {
-            if rng.gen_range(0, 100) < 50 {
-                cells.insert((x, y));
-            }
-        }
-    }
+    let mut speed = 20.0;
 
-    loop {
-        if let Ok("quit") | Err(TryRecvError::Disconnected) = rx.try_recv() {
-            break;
-        }
-
+    'app: loop {
+        let mut cells = CellGrid::new();
         let (w, h) = termion::terminal_size().unwrap();
-
-        write!(screen, "{}", termion::cursor::Goto(1, 1)).unwrap();
-
-        let mut cell_string = String::with_capacity((w * h) as usize);
-        for y in 1..=h {
-            for x in 1..=w {
-                if cells.contains(&(x as isize, y as isize)) {
-                    cell_string.push('█');
-                } else {
-                    cell_string.push(' ');
+        let mut rng = thread_rng();
+        let threshold = rng.gen_range(0, 100);
+        for x in (1..=w).map(|x| x as isize) {
+            for y in (1..=h).map(|x| x as isize) {
+                if rng.gen_range(0, 100) < threshold {
+                    cells.insert((x, y));
                 }
             }
         }
-        write!(screen, "{}", cell_string).unwrap();
-        screen.flush().unwrap();
 
-        let handle = thread::spawn(move || {
-            step(&cells)
-        });
+        'restart: loop {
+            match rx.try_recv() {
+                Ok("quit") | Err(TryRecvError::Disconnected) => break 'app,
+                Ok("restart") => break 'restart,
+                Ok("speed+") => speed *= 1.2,
+                Ok("speed-") => speed /= 1.2,
+                _ => (),
+            }
 
-        thread::sleep(Duration::from_millis(1000 / 30));
+            let (w, h) = termion::terminal_size().unwrap();
 
-        cells = handle.join().unwrap();
+            write!(screen, "{}", termion::cursor::Goto(1, 1)).unwrap();
+
+            let mut cell_string = String::with_capacity((w * h) as usize);
+            for y in 1..=h {
+                for x in 1..=w {
+                    if cells.contains(&(x as isize, y as isize)) {
+                        cell_string.push('█');
+                    } else {
+                        cell_string.push(' ');
+                    }
+                }
+            }
+            write!(screen, "{}", cell_string).unwrap();
+            screen.flush().unwrap();
+
+            let handle = thread::spawn(move || {
+                step(&cells)
+            });
+
+            thread::sleep(Duration::from_secs_f64(1.0 / speed));
+
+            cells = handle.join().unwrap();
+        }
     }
 
     key_handler.join().unwrap();
@@ -79,6 +94,7 @@ fn main() {
 fn step(last: &CellGrid) -> CellGrid {
     let mut check = HashMap::new();
 
+    // for every cell in a living cell's neighbourhood, add 1 to the neighbour count
     for cell in last.iter() {
         for x in -1..=1 {
             for y in -1..=1 {
